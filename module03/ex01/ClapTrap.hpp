eea30690a4cd3e1fb7e67bd1ac4c675560caea64
Adam BEN MCHICHI <adben-mc@e1r2p16.clusters.42paris.fr>
2022-10-02 19:41:38 +0200

    ex01

---
+++ module03/ex01/ClapTrap.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adben-mc <adben-mc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/01 22:06:23 by adben-mc          #+#    #+#             */
+/*   Updated: 2022/10/02 19:33:16 by adben-mc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_H
+# define CLAPTRAP_H
+
+#include <iostream>
+
+class ClapTrap {
+
+	public : 
+		ClapTrap(void);
+		ClapTrap(std::string const name);
+		~ClapTrap(void);
+		ClapTrap(ClapTrap const &trap);
+		void attack(const std::string &target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+		ClapTrap operator=(ClapTrap const &rhs);
+
+
+	protected : 
+		std::string	_name;
+		int			_hit_point;
+		int			_energy_point;
+		int			_attack_damage;
+
+};
+
+// std::ostream &operator<<( std::ostream &out, ClapTrap const &fixed);
+
+#endif
---
+++ module03/ex01/main.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adben-mc <adben-mc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/02 00:16:53 by adben-mc          #+#    #+#             */
+/*   Updated: 2022/10/02 19:40:55 by adben-mc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+int main(void)
+{
+	// ClapTrap Trap;
+	// ClapTrap John("John");
+	
+	// John.attack("bob");
+	// John.attack("bob");
+	// John.attack("bob");
+	// John.attack("bob");
+	// John.attack("bob");
+	// John.attack("bob");
+	// John.attack("bob");
+	// John.attack("bob");
+	// John.attack("bob");
+	// John.attack("bob");
+	// John.beRepaired(15);
+
+	// John.takeDamage(5);
+	// John.takeDamage(4);
+	// John.takeDamage(1);
+	// John.takeDamage(1);
+	// John.takeDamage(1);
+	// John.attack("Jean");
+	// John.beRepaired(5);
+
+	std::cout << std::endl << "-----------------" << std::endl << std::endl;
+
+	ScavTrap Bob("bob");
+	Bob.attack("John");
+	Bob.guardGate();
+}
