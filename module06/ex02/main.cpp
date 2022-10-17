b3d34b87abc802905e3277bc3db37cdfa92c7c6b
Adam BEN MCHICHI <adben-mc@e2r5p6.clusters.42paris.fr>
2022-10-17 17:22:41 +0200

    ex02

--- module06/ex02/main.cpp
+++ module06/ex02/main.cpp
@@ -12,12 +12,15 @@ Base * generate(void)
 	switch (r)
 	{
 		case 0:
+			std::cout << "A is created" << std::endl;
 			return (new A());
 			break;
 		case 1:
+			std::cout << "B is created" << std::endl;
 			return (new B());
 			break;
 		case 2:
+			std::cout << "C is created" << std::endl;
 			return (new C());
 			break;
 		default:
@@ -30,24 +33,28 @@ void identify(Base* p)
 {
 	//Elle affiche le véritable type de l’objet pointé par p : "A", "B" ou "C".
 	(void)p;
+	std::cout << "Conversion by PTR" << std::endl;
 
 	try {
 		A &a = dynamic_cast<A &>(*p);
-		std::cout << "p is an A";
+		std::cout << "It's A" << std::endl;
+		static_cast<void>(a);
 	}
-	catch (std::string &e) {}
+	catch (std::exception &e) {}
 
 	try {
 		B &b = dynamic_cast<B &>(*p);
-		std::cout << "p is an B";
+		std::cout << "It's B" << std::endl;
+		static_cast<void>(b);
 	}
-	catch (std::string &e) {}
+	catch (std::exception &e) {}
 
 	try {
 		C &c = dynamic_cast<C &>(*p);
-		std::cout << "p is an C";
+		std::cout << "It's C" << std::endl;
+		static_cast<void>(c);
 	}
-	catch (std::string &e) {}
+	catch (std::exception &e) {}
 }
 
 void identify(Base& p)
@@ -55,30 +62,50 @@ void identify(Base& p)
 	//Elle affiche le véritable type de l’objet pointé par p : "A", "B" ou "C". 
 	//Utiliser un pointeur dans cette fonction est interdit.
 	(void)p;
-
+	std::cout << "Conversion by REF" << std::endl;
+	
 	try {
 		A &a = dynamic_cast<A &>(p);
-		std::cout << "p is an A";
+		std::cout << "It's A" << std::endl;
+		static_cast<void>(a);
 	}
-	catch (std::string &e) {}
+	catch (std::exception &e) {}
 
 	try {
 		B &b = dynamic_cast<B &>(p);
-		std::cout << "p is an B";
+		std::cout << "It's B" << std::endl;
+		static_cast<void>(b);
 	}
-	catch (std::string &e) {}
+	catch (std::exception &e) {}
 
 	try {
 		C &c = dynamic_cast<C &>(p);
-		std::cout << "p is an C";
+		std::cout << "It's C" << std::endl;
+		static_cast<void>(c);
 	}
-	catch (std::string &e) {}
+	catch (std::exception &e) {}
 }
 
 int main()
 {
 	srand(time(NULL));
 
+	std::cout << "\tRandomly generated" << std::endl;
+	Base *base = generate();
+	identify(base);
+	identify(*base);
+
+	std::cout << std::endl << "\tCreation of A" << std::endl;
+
 	A a = A();
 	identify(a);
+	identify(&a);
+	std::cout << std::endl << "\tCreation of B" << std::endl;
+	B b = B();
+	identify(b);
+	identify(&b);
+	std::cout << std::endl << "\tCreation of C" << std::endl;
+	C c = C();
+	identify(c);
+	identify(&c);
 }
